1705ee445bf2d52713903c41d87377fcc0deaeeb
choidy180 <52563196+choidy180@users.noreply.github.com>
2020-07-10 17:51:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0 
-  }
-  /* 플러스 */
-  plus = () => {
-    this.setState(current => ({count: current.count +1}));
-    console.log("plus");
+    isLoding: true,
+    movies: []
   };
-  /* 마이너스 */
-  minus = () => {
-    this.setState(current => ({count: current.count -1}));
-    console.log("minus");
-  };
-  componentDidMount() {
-    console.log("component render");
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoding: false})
+    },3000)
   }
-
-  componentDidUpdate(){
-    /* 업데이트 되면 실행되는 함수 */
-    console.log("I just updated");
-  }
-  componentWillUnmount(){
-    console.log("Goodbye, cruel world");
-  }
-  /* 리엑트 컴포넌스에서 사용하는 유일한 함수 render */
   render() {
-    console.log("I'm rendering");
-    return (
-    <div>
-        <h1>Im a class Component {this.state.count}</h1>
-        <button onClick={this.plus}>Plus</button> &nbsp;&nbsp;&nbsp;
-        <button onClick={this.minus}>Minus</button>
-    </div>
+    const {isLoding} = this.state;
+    return(
+      <div>
+        {isLoding ? "Loding..." : "We are ready"}
+      </div>
     );
   }
 }
