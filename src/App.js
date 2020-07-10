273ebc4af4c26c08946f6c1209f487988cc1dd40
choidy180 <52563196+choidy180@users.noreply.github.com>
2020-07-10 17:39:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0 
   }
-  /* 자바스크립트의 주석과 같다 */
+  /* 플러스 */
   plus = () => {
     this.setState(current => ({count: current.count +1}));
     console.log("plus");
   };
+  /* 마이너스 */
   minus = () => {
     this.setState(current => ({count: current.count -1}));
     console.log("minus");
   };
+  componentDidMount() {
+    console.log("component render");
+  }
+
+  componentDidUpdate(){
+    /* 업데이트 되면 실행되는 함수 */
+    console.log("I just updated");
+  }
+  componentWillUnmount(){
+    console.log("Goodbye, cruel world");
+  }
+  /* 리엑트 컴포넌스에서 사용하는 유일한 함수 render */
   render() {
+    console.log("I'm rendering");
     return (
     <div>
         <h1>Im a class Component {this.state.count}</h1>
