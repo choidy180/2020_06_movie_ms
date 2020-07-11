f44cec46951b3208b01db86c8afcab951e9b9e32
choidy180 <choidy180@naver.com>
2020-07-11 18:51:22 +0900

    Updates

---
+++ precache-manifest.a9a4e142a4fbc11a926712354c973036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2936fbcd1adfd661b0d2b1b34f10c29",
+    "url": "/choidy180/movie_ms2020/index.html"
+  },
+  {
+    "revision": "750f1be906646bb9a7a0",
+    "url": "/choidy180/movie_ms2020/static/css/main.4681bd19.chunk.css"
+  },
+  {
+    "revision": "41ab86e84378229dc9a8",
+    "url": "/choidy180/movie_ms2020/static/js/2.c3df327e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/choidy180/movie_ms2020/static/js/2.c3df327e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750f1be906646bb9a7a0",
+    "url": "/choidy180/movie_ms2020/static/js/main.b5f19ef5.chunk.js"
+  },
+  {
+    "revision": "41564a3e9a7ae615b193",
+    "url": "/choidy180/movie_ms2020/static/js/runtime-main.aac4626f.js"
+  }
+]);
